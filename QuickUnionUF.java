a20b8f05049e3b3818c7f7a15f9590c30fb9f209
Pranit Bauva <pranit.bauva@gmail.com>
2015-06-23 11:47:06 +0530

    Improve the algorithm for union of the list.

--- QuickUnionUF.java
+++ QuickUnionUF.java
@@ -1,9 +1,12 @@
 public class QuickUnionUF{
     int id[];
+    int sz[];
     public QuickUnionUF(int N){
         id = new int[N];
+        sz = new int[N];
         for(int i = 0; i < N; i++){
             id[i] = i;
+            sz[i] = 0;
         }
     }
 
@@ -19,10 +22,21 @@ public class QuickUnionUF{
         else
             return false;
     }
-    public int union(int p, int q){
+    public void union(int p, int q){
         int i = root(p);
         int j = root(q);
-        id[i] = j;
+        if(i == j)
+            return;
+        else{
+            if(sz[i] < sz[j]){
+                id[i] = j;
+                sz[i] += sz[j];
+            }
+            else{
+                id[j] = i;
+                sz[j] += sz[i];
+            }
+        }
     }
 }
 
